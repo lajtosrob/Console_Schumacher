a009bbb3ed810b9ec3352c758d277f2760244bb8
Lifebook727 <Lifebook727@DESKTOP-DO4UU49>
2023-12-11 17:16:53 +0100

    Add project files.

---
+++ DataSource/Eredmeny.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Schumacher.DataSource
+{
+    internal class Eredmeny
+    {
+        string date;
+        string grandprix;
+        int position;
+        int laps;
+        int points;
+        string team;
+        string status;
+
+        public Eredmeny(string date, string grandprix, int position, int laps, int points, string team, string status)
+        {
+            this.date = date;
+            this.grandprix = grandprix;
+            this.position = position;
+            this.laps = laps;
+            this.points = points;
+            this.team = team;
+            this.status = status;
+        }
+
+        public string Date { get => date; set => date = value; }
+        public string Grandprix { get => grandprix; set => grandprix = value; }
+
+        public int Position { get => position; set => position = value; }   
+        public int Laps { get => laps; set => laps = value; }
+        public int Points { get => points; set => points = value; }
+        public string Team { get => team; set => team = value; }
+        public string Status { get => status; set => status = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Console_Schumacher.DataSource;
+
+namespace Console_Schumacher
+{
+    internal class Program
+    {
+        static List<Eredmeny> eredmenyek = new List<Eredmeny>();   
+        static void Main(string[] args)
+
+            // 2. feladat
+        {
+            StreamReader sr = new StreamReader(".\\DataSource\\schumacher.csv");
+
+            sr.ReadLine();
+
+            while(!sr.EndOfStream)
+            {
+                string[] line = sr.ReadLine().Split(";");
+
+                Eredmeny adatsor = new Eredmeny(
+                    line[0],
+                    line[1],
+                    int.Parse(line[2]),
+                    int.Parse(line[3]),
+                    int.Parse(line[4]),
+                    line[5],
+                    line[6]
+                    ) ;
+
+                eredmenyek.Add(adatsor);
+            }
+
+            sr.Close();
+
+            // 3. feladat
+
+            Console.WriteLine($"3. feladat: {eredmenyek.Count()}");
+
+            // 4. feladat
+
+            Console.WriteLine($"4. feadat: Magyar nagydíj helyezései: ");
+            eredmenyek.Where(x => x.Grandprix == "Hungarian Grand Prix" && x.Status == "Finished").ToList().ForEach(x => Console.WriteLine($"{x.Date} : {x.Position}. hely"));
+
+            // 5. feladat
+
+            Dictionary<string, int> statisztika = eredmenyek.Where(x => x.Status != "Finished" && x.Status != "+1 Lap" && x.Status != "+2 Lap").GroupBy(x => x.Status).ToDictionary(x => x.Key, x => x.Count());
+
+            Console.WriteLine("5. feladat");
+
+            foreach (var item in statisztika)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+        }
+    }
+}
